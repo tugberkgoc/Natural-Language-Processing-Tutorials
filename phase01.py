af7ae0e4f5dca177fbd549e6818190bc7fb9a91a
tugberkgoc <tugberkgoc@gmail.com>
2019-03-11 16:41:36 +0300

    :fire:Done Phase 01

--- phase01.py
+++ phase01.py
@@ -2,74 +2,78 @@ import nltk
 from nltk.probability import FreqDist
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
+import pandas as pd
 
 ps = PorterStemmer()
 
 nltk.download('punkt')
 nltk.download('stopwords')
 
-text = open("text.txt", "r")
+df = pd.read_csv("Reviews.csv")
+saved_column = df['Text']
 
-readTheText = text.read()
+for text in saved_column:
 
-tokenized = nltk.word_tokenize(readTheText)
+    readTheText = text
 
-tokenized_sentence = nltk.sent_tokenize(readTheText)
+    tokenized = nltk.word_tokenize(readTheText)
 
-distribution = FreqDist(tokenized)
+    tokenized_sentence = nltk.sent_tokenize(readTheText)
 
-stop_words = set(stopwords.words("english"))
+    distribution = FreqDist(tokenized)
 
-filtered_sentence = list()
+    stop_words = set(stopwords.words("english"))
 
-for word in tokenized:
-    if word not in stop_words:
-        filtered_sentence.append(word)
+    filtered_sentence = list()
 
-print("2)")
-print("================================================================")
-print("Tokenized Text")
-print("================================================================")
-print(tokenized)
+    for word in tokenized:
+        if word not in stop_words:
+            filtered_sentence.append(word)
 
-print("3)")
-print("================================================================")
-print("The words that are not stop words. ")
-print("================================================================")
-print(filtered_sentence)
+    print("2)")
+    print("================================================================")
+    print("Tokenized Text")
+    print("================================================================")
+    print(tokenized)
 
-stem_words = list()
-print("4)")
-print("================================================================")
-print("STEM")
-print("================================================================")
-for x in filtered_sentence:
-    stem_words.append(ps.stem(x))
-print(stem_words)
+    print("3)")
+    print("================================================================")
+    print("The words that are not stop words. ")
+    print("================================================================")
+    print(filtered_sentence)
 
-print("5)")
-print("================================================================")
-print("Basic information about words. ")
-print("================================================================")
-print(FreqDist(stem_words))
+    stem_words = list()
+    print("4)")
+    print("================================================================")
+    print("STEM")
+    print("================================================================")
+    for x in filtered_sentence:
+        stem_words.append(ps.stem(x))
+    print(stem_words)
 
-print("6)")
-print("================================================================")
-print("MOST COMMON 10 STEM")
-print("================================================================")
-stem_most_common_words = FreqDist(stem_words).most_common(10)
-print(stem_most_common_words)
+    print("5)")
+    print("================================================================")
+    print("Basic information about words. ")
+    print("================================================================")
+    print(FreqDist(stem_words))
 
-print("7)")
-print("================================================================")
-print("VISUALIZE")
-print("================================================================")
-(FreqDist(stem_words)).plot(30, cumulative=False)
+    print("6)")
+    print("================================================================")
+    print("MOST COMMON 10 STEM")
+    print("================================================================")
+    stem_most_common_words = FreqDist(stem_words).most_common(10)
+    print(stem_most_common_words)
 
-print("8)")
-print("================================================================")
-print("MORE THAN 10 LETTERS")
-print("================================================================")
-for x in tokenized:
-    if len(x) >= 10:
-        print(x)
+    print("7)")
+    print("================================================================")
+    print("VISUALIZE")
+    print("================================================================")
+    (FreqDist(stem_words)).plot(30, cumulative=False)
+
+    print("8)")
+    print("================================================================")
+    print("MORE THAN 10 LETTERS")
+    print("================================================================")
+    for x in tokenized:
+        if len(x) >= 10:
+            print(x)
