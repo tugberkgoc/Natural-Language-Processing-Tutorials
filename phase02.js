360d477c4d2b04a6c99bf77a7c6b4c23f28213eb
tugberkgoc <tugberkgoc@gmail.com>
2019-03-17 12:46:02 +0300

    :sparkles:Add Read File Function for StopWords

--- phase02.js
+++ phase02.js
@@ -1,15 +1,16 @@
-const natural = require("natural");
+const natural = require("natural")
 const util = require('util')
 const nGram = require('n-gram')
 const fs = require('fs')
 
 let text = ''
+let stopWords = []
 
 try {
-  let data = fs.readFileSync('file.txt', 'utf8');
-  text = data.toString()
+  text = fs.readFileSync('file.txt', 'utf8').toString()
+  stopWords = fs.readFileSync('stopWords.txt', 'utf-8').toString().split()[0]
 } catch (e) {
-  console.log('Error: ', e.stack);
+  console.log('Error: ', e.stack)
   process.exit()
 }
 
@@ -21,182 +22,6 @@ try {
 preprocess = (text) => {
   let tokenizer = new natural.WordTokenizer()
   let tokenizeText = tokenizer.tokenize(text.toLowerCase())
-  let stopWords = [
-    "a",
-    "about",
-    "above",
-    "after",
-    "again",
-    "against",
-    "all",
-    "am",
-    "an",
-    "and",
-    "any",
-    "are",
-    "aren't",
-    "as",
-    "at",
-    "be",
-    "because",
-    "been",
-    "before",
-    "being",
-    "below",
-    "between",
-    "both",
-    "but",
-    "by",
-    "can't",
-    "cannot",
-    "could",
-    "couldn't",
-    "did",
-    "didn't",
-    "do",
-    "does",
-    "doesn't",
-    "doing",
-    "don't",
-    "down",
-    "during",
-    "each",
-    "few",
-    "for",
-    "from",
-    "further",
-    "had",
-    "hadn't",
-    "has",
-    "hasn't",
-    "have",
-    "haven't",
-    "having",
-    "he",
-    "he'd",
-    "he'll",
-    "he's",
-    "her",
-    "here",
-    "here's",
-    "hers",
-    "herself",
-    "him",
-    "himself",
-    "his",
-    "how",
-    "how's",
-    "i",
-    "i'd",
-    "i'll",
-    "i'm",
-    "i've",
-    "if",
-    "in",
-    "into",
-    "is",
-    "isn't",
-    "it",
-    "it's",
-    "its",
-    "itself",
-    "let's",
-    "me",
-    "more",
-    "most",
-    "mustn't",
-    "my",
-    "myself",
-    "no",
-    "nor",
-    "not",
-    "of",
-    "off",
-    "on",
-    "once",
-    "only",
-    "or",
-    "other",
-    "ought",
-    "our",
-    "ours",
-    "ourselves",
-    "out",
-    "over",
-    "own",
-    "same",
-    "shan't",
-    "she",
-    "she'd",
-    "she'll",
-    "she's",
-    "should",
-    "shouldn't",
-    "so",
-    "some",
-    "such",
-    "than",
-    "that",
-    "that's",
-    "the",
-    "their",
-    "theirs",
-    "them",
-    "themselves",
-    "then",
-    "there",
-    "there's",
-    "these",
-    "they",
-    "they'd",
-    "they'll",
-    "they're",
-    "they've",
-    "this",
-    "those",
-    "through",
-    "to",
-    "too",
-    "under",
-    "until",
-    "up",
-    "very",
-    "was",
-    "wasn't",
-    "we",
-    "we'd",
-    "we'll",
-    "we're",
-    "we've",
-    "were",
-    "weren't",
-    "what",
-    "what's",
-    "when",
-    "when's",
-    "where",
-    "where's",
-    "which",
-    "while",
-    "who",
-    "who's",
-    "whom",
-    "why",
-    "why's",
-    "with",
-    "won't",
-    "would",
-    "wouldn't",
-    "you",
-    "you'd",
-    "you'll",
-    "you're",
-    "you've",
-    "your",
-    "yours",
-    "yourself",
-    "yourselves"
-  ];
   return tokenizeText.filter(x => !stopWords.includes(x))
 }
 
@@ -214,7 +39,6 @@ mostFrequent = (tokenizeWithoutStopWords, howManyWords) => {
   let result = []
   let ps = []
   let map = new Map()
-  const sortStringValues = (a, b) => (a[1] === b[1] ? 0 : a[1] > b[1] ? -1 : 1)
 
   tokenizeWithoutStopWords.forEach(x => ps.push(natural.PorterStemmer.stem(x)))
 
@@ -222,7 +46,7 @@ mostFrequent = (tokenizeWithoutStopWords, howManyWords) => {
     map.has(x) ? map.set(x, map.get(x) + 1) : map.set(x, 1)
   })
 
-  let lastOne = [...map].sort(sortStringValues).slice(0, howManyWords) // You can use new Map([...map].sort(sortStringValues)) to avoid creating object at the end
+  let lastOne = [...map].sort((a, b) => (a[1] === b[1] ? 0 : a[1] > b[1] ? -1 : 1)).slice(0, howManyWords) // You can use new Map([...map].sort(sortStringValues)) to avoid creating object at the end
 
   lastOne.forEach((val) => {
     result.push(val)
@@ -231,9 +55,9 @@ mostFrequent = (tokenizeWithoutStopWords, howManyWords) => {
   return result
 }
 
-console.log(mostFrequent(preProcessed, 5).forEach((val) => console.log(val)))
+mostFrequent(preProcessed, 5).forEach((val) => console.log(val))
 
-displayNgrams = (preProcessed, n) => {
-}
+// displayNgrams = (preProcessed, n) => {
+// }
 
 // console.log(displayNgrams(preProcessed, 5))
