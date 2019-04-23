f147477826494528c61a2aa9670843963a57ef41
Kadir <kadirakgul255@gmail.com>
2019-04-23 19:51:20 +0300

    Defining function

--- python/phase03.py
+++ python/phase03.py
@@ -4,13 +4,18 @@ from nltk.corpus import brown
 print('=============================================PART 01 =====================================================')
 print('Constructs a lexicon that maps words of the English language to lexical categories. (1000 items)')
 print('==========================================================================================================')
-temp = {}
-for word, pos in sorted(key for key in brown.tagged_words()[:1000]):
-    if word not in temp:
-        temp[word] = pos
 
-for x in temp:
-    print(f'{x:<20} {temp[x]}')
+def constructLexicon():
+    temp = {}
+    for word, pos in sorted(key for key in brown.tagged_words()[:1000]):
+        if word not in temp:
+            temp[word] = pos
+
+    for x in temp:
+        print(f'{x:<20} {temp[x]}')
+
+
+constructLexicon()
 
 print('=============================================PART 02 =====================================================')
 print('Constructs a grammar that denes the structure of a sentence.')
@@ -25,9 +30,12 @@ N -> 'elephant' | 'pajamas'
 V -> 'shot'
 P -> 'in'
 """)
+def genarateGrammer(sent):
+    parser = nltk.ChartParser(grammar, trace = 1)
+    for tree in parser.parse(sent):
+        print(tree)
+
+
 
 example_sentence = ['I', 'shot', 'an', 'elephant', 'in', 'my', 'pajamas']
-
-parser = nltk.ChartParser(grammar)
-for tree in parser.parse(example_sentence):
-    print(tree)
+genarateGrammer(example_sentence)
