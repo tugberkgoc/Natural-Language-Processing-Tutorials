99f58a2c7ebefae563e9771cae4a5fc8e4ae63a0
tugberkgoc <tugberkgoc@gmail.com>
2019-05-13 22:25:22 +0300

    :fire: Phase04 is completed

--- python/phase04.py
+++ python/phase04.py
@@ -1,84 +1,128 @@
-import nltk
-from nltk.corpus import stopwords
-import pandas as pd
 import re
-import numpy as np
-from sklearn.feature_extraction.text import CountVectorizer
-
+import pandas as pd
+from sklearn.svm import SVC
+from nltk import word_tokenize
+from nltk.corpus import stopwords
+from sklearn.metrics import r2_score
+from nltk.corpus import wordnet as wn
 from sklearn.metrics import accuracy_score
-from sklearn.svm import LinearSVC
-
 from sklearn.naive_bayes import GaussianNB
-from sklearn.metrics import confusion_matrix
+from nltk.stem.porter import PorterStemmer
+from sklearn.linear_model import LinearRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics import classification_report, confusion_matrix
+
+# nltk.download('wordnet')
 
 # MAIN
-df = pd.read_csv("Reviews.csv")
-rr = pd.read_csv("Train.csv")
-texts_train = df['Text']
-score_train = df['Score']
-score_test = rr['Score']
-texts_test = rr['Text']
+df = pd.read_csv("../test.csv")
+
+reviews = df.iloc[:, 9]
+scores = df.iloc[:, 6]
+
+reviews_array = []
+for i in reviews:
+    reviews_array.append(i)
 
 
-def word_extraction(sentence):
-    stop_words = set(stopwords.words("english"))
-    ignore = stop_words
-    words = re.sub("[^\w]", " ", sentence).split()
-    cleaned_text = [w.lower() for w in words if w not in ignore]
-    cleaned_text = [x.lower() for x in cleaned_text if x.isalpha()]
-    return cleaned_text
+def clean(given_corpus):
+    cleaned_text = re.sub('[^a-zA-Z]', ' ', given_corpus).lower().split()
+    cleaned_corpus = [PorterStemmer().stem(word) for word in cleaned_text if
+                      not word in set(stopwords.words('english'))]
+    return ' '.join(cleaned_corpus)
 
 
-def tokenize(sentences):
-    words = []
-    for sentence in sentences:
-        w = word_extraction(sentence)
-        words.extend(w)
+corpus = []
+for i in reviews_array:
+    corpus.append(clean(i))
 
-    words = sorted(list(set(words)))
-    return words
+print('============================================= Phase 04 ====================================================')
+print('Bag of Words')
+print('===========================================================================================================')
+cv = CountVectorizer(max_features=10000)
+X = cv.fit_transform(corpus).toarray()
+print(X)
+y = scores
 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=0)
 
-def generate_bow(allsentences):
-    vocab = tokenize(allsentences)
-    print("Word List for Document \n{0} \n".format(vocab));
+print('Gaussian NB')
+print('===========================================================================================================')
 
-    for sentence in allsentences:
-        words = word_extraction(sentence)
-        bag_vector = np.zeros(len(vocab))
-        for w in words:
-            for i, word in enumerate(vocab):
-                if word == w:
-                    bag_vector[i] += 1
+classifier = GaussianNB()
+classifier.fit(X_train, y_train)
+y_prediction_naive_bayes = classifier.predict(X_test)
 
-        print("{0}\n{1}\n".format(sentence, np.array(bag_vector)))
+print('Accuracy score for Naive Bayes')
+print('===========================================================================================================')
 
+print(accuracy_score(y_prediction_naive_bayes, y_test))
+print(classification_report(y_test, y_prediction_naive_bayes))
 
-generate_bow(texts_train)
-# generate_bow(allsentences[8:10])
+print('Linear Regression')
+print('===========================================================================================================')
 
-vectorizer = CountVectorizer()
-training_features = vectorizer.fit_transform(texts_train)
-test_features = vectorizer.transform(texts_test)
+clf = LinearRegression(normalize=True)
+clf.fit(X_train, y_train)
+y_prediction_linear = clf.predict(X_test)
 
-# print(training_features.toarray())
+print("r^2 score for Linear")
+print('===========================================================================================================')
 
-# Training
-model = LinearSVC()
-model.fit(training_features, score_train)
-y_pred = model.predict(test_features)
+print(r2_score(y_test, y_prediction_linear))
 
-# Evaluation
-acc = accuracy_score(score_test, y_pred)
+print('SVM')
+print('===========================================================================================================')
 
-print("Accuracy on the IMDB dataset: {:.2f}".format(acc * 100))
+model = SVC(kernel='linear')
+model.fit(X_train, y_train)
+y_prediction_SVM = model.predict(X_test)
 
+print('Confusion Matrix for SVM')
+print('===========================================================================================================')
 
-# classifier = GaussianNB()
-# classifier.fit(texts_train, score_train)
-#
-# y_pred = classifier.predict(texts_test)
-#
-# cm = confusion_matrix(texts_test, y_pred)
-#
-# # print(cm)
+print(confusion_matrix(y_test, y_prediction_SVM))
+print(accuracy_score(y_prediction_SVM, y_test))
+print(classification_report(y_test, y_prediction_SVM))
+
+print('KNN')
+print('===========================================================================================================')
+classifier = KNeighborsClassifier(n_neighbors=5, metric='minkowski', p=2)
+classifier.fit(X_train, y_train)
+
+print('Predicted test results')
+print('===========================================================================================================')
+y_prediction_knn = classifier.predict(X_test)
+
+print('Making the Confusion Matrix')
+print('===========================================================================================================')
+cm = confusion_matrix(y_test, y_prediction_knn)
+
+print(accuracy_score(y_prediction_knn, y_test))
+
+print('WORD NET')
+print('===========================================================================================================')
+
+ex_01 = word_tokenize("People can understand complicated situations.")
+ex_02 = word_tokenize("Monkey can not think as people do in complicated situations.")
+
+sum_result = 0
+sim_array = []
+
+for r in ex_01:
+    for s in ex_02:
+        n = wn.synsets(r)
+        g = wn.synsets(s)
+        for i in g:
+            if i is not None:
+                if type(i.path_similarity(n[0])) is float:
+                    sim_array.append(i.path_similarity(n[0]))
+                    print(i.path_similarity(n[0]))
+
+result_list = list(dict.fromkeys(sim_array))
+print(result_list)
+
+for i in result_list:
+    sum_result = sum_result + i
